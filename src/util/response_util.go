1b78508b151becfcd0cd7cde55599acf439e3e05
Anew Eiamsuwansai <anew.el@brightexpert.co.th>
2022-07-01 10:38:36 +0700

    remove struct

--- src/util/response_util.go
+++ src/util/response_util.go
@@ -2,9 +2,6 @@ package util
 
 import "github.com/gin-gonic/gin"
 
-type ResponseUtil struct {
-}
-
 func ResponseSuccess(ctx *gin.Context, data any) {
 	ctx.JSON(200, gin.H{
 		"message": "success",
