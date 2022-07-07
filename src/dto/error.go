943da676be4c8566584c0f4bcd13ba5a5e21ef4b
Anew Eiamsuwansai <anew.el@brightexpert.co.th>
2022-07-07 09:46:37 +0700

    bind BuildMessage method to errors structs

--- src/dto/error.go
+++ src/dto/error.go
@@ -1,11 +1,32 @@
 package dto
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type ValidationError struct {
-	Key         string `json:"key"`
-	Value       any    `json:"value"`
-	Description string `json:"description"`
+	Key         string `json:"key,omitempty"`
+	Value       any    `json:"value,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+func (v *ValidationError) BuildMessage() string {
+	msgBytes, err := json.Marshal(v)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(msgBytes)
 }
 
 type ExternalServiceError struct {
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
+}
+
+func (e *ExternalServiceError) BuildMessage() string {
+	msgBytes, err := json.Marshal(e)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(msgBytes)
 }
